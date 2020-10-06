d9066ede37c934938b6dcf0688ca745e6ed0c8d5
Scott Mohorn <scottmohorn@gmail.com>
2020-10-06 17:21:50 -0600

    made some components

---
+++ client/src/jobcomponents/JobBoard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import JobCard from "./JobCard"
+
+const JobBoard = () => {
+  return(
+    <div>
+    <h1>JobBoard</h1>
+    <JobCard />
+    </div>
+  )
+} 
+
+export default JobBoard
